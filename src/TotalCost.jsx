40cef1100a200d320050f1dfa4a6276d5c57cb29
Julia Zdzitowiecka <jul.zdzitowiecka@gmail.com>
2026-01-08 07:29:33 -0500

    create conference event planer

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -2,26 +2,23 @@ import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
-
-  return (
-    <div className="pricing-app">
-      <div className="display_box">
-        <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
-        </div>
-        <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
-         
-            <div>
-             
+    return (
+        <div className="pricing-app">
+            <div className="display_box">
+                <div className="header">
+                    <p className="preheading"><h3>Total cost for the event</h3></p>
+                </div>
+                <div>
+                    <h2 id="pre_fee_cost_display" className="price">
+                        Total Cost: ${totalCosts}
+                    </h2>
+                    <div>
+                        {ItemsDisplay}
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TotalCost;
